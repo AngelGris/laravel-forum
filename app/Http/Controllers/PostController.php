8708b0f3c03911b2bf6f021f8ddb26d368dc07c4
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-12-24 19:10:31 -0300

    Update topic's last_post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 use App\Post;
+use App\Topic;
 
 class PostController extends Controller
 {
@@ -13,18 +15,22 @@ class PostController extends Controller
      */
     public function save(Request $request)
     {
-        $user = Auth::user();
-
         $this->validate($request, [
             'post'      => 'required|string',
             'topic_id'  => 'required|integer'
         ]);
 
+        $user = Auth::user();
+        $topic = Topic::find($request->input('topic_id'));
+
         Post::create([
             'user_id'   => $user->id,
-            'topic_id'  => $request->input('topic_id'),
+            'topic_id'  => $topic->id,
             'post'      => $request->input('post')
         ]);
+
+        $topic->last_post = Carbon::now();
+        $topic->save();
 
         return redirect()->back()
                 ->with('message', 'Post created');
