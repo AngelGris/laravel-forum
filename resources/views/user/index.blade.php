a80183f06b683fad13f318cb855ea8b3fbd56a74
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-12-26 10:02:15 -0300

    Profile topics and posts

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,9 +9,9 @@
             <dt>Joined:</dt> <dd>{{ $user->created_at->diffForHumans() }}</dd>
             <dt>Last active:</dt> <dd>{{ $user->last_activity->diffForHumans() }}</dd>
             <dt>Total topics:</dt>
-            <dd>{{ count($user->topics) }} ({{ $user->percentage_topics }}% of all topics / {{ $user->topics_per_day }} topics per day)</dd>
+            <dd><a href="{{ route('profile.topics', $user->id) }}">{{ count($user->topics) }} ({{ $user->percentage_topics }}% of all topics / {{ $user->topics_per_day }} topics per day)</a></dd>
             <dt>Total posts:</dt>
-            <dd>{{ count($user->posts) }} ({{ $user->percentage_posts }}% of all posts / {{ $user->posts_per_day }} posts per day)</dd>
+            <dd><a href="{{ route('profile.posts', $user->id) }}">{{ count($user->posts) }} ({{ $user->percentage_posts }}% of all posts / {{ $user->posts_per_day }} posts per day)</a></dd>
             @if(!empty($user->signature))
             <dt>Signature:</dt>
             <dd>{{ $user->signature }}</dd>
